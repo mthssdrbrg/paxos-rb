1898360a46f611a90ffa6bc16ab2ee3110795de5
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-01-15 16:53:46 +0000

    Removed NodeFactory

--- lib/paxos.rb
+++ lib/paxos.rb
@@ -8,15 +8,4 @@ module Paxos
 
 	class ValueMismatchError < ArgumentError
 	end
-
-	module NodeFactory
-
-		def self.create!(proposer_uid, leader_uid, quorum_size, &resolution_callback)
-			proposer = Proposer.new(proposer_uid, quorum_size)
-			acceptor = Acceptor.new
-			learner = Learner.new(quorum_size)
-
-			Node.new(proposer, acceptor, learner, resolution_callback)
-		end
-	end
 end
