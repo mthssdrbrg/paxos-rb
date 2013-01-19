3febdc1aebb4853be81a308cdd26bf70ad53747d
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-01-19 14:03:06 +0100

    Added implementation of ProposalId

--- lib/paxos.rb
+++ lib/paxos.rb
@@ -6,6 +6,22 @@ require 'paxos/node'
 
 module Paxos
 
+  class ProposalId < Struct.new(:number, :uid)
+    include Comparable
+
+    def <=>(other)
+      return nil if other.nil? or not (other.respond_to?(:number) && other.respond_to?(:uid))
+
+      if number > other.number
+        1
+      elsif number < other.number
+        -1
+      elsif number == other.number
+        uid <=> other.uid
+      end
+    end
+  end
+
 	class ValueMismatchError < ArgumentError
 	end
 end
